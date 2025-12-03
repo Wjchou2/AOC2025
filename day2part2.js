190c0dce90eec98a93ee1a0c88903ff781ff98ba
WilliamChou123 <wchou28@shschools.org>
2025-12-03 11:59:53 -0800

    day 3

--- day2part2.js
+++ day2part2.js
@@ -6,11 +6,9 @@ Array.from(lines).forEach(function (elm) {
     elm = elm.split("-");
     const elm0 = Number(elm[0]);
     const elm1 = Number(elm[1]);
-    // console.log(elm0, elm1);
     let currentCheck = elm0;
     for (let i = 0; i < elm1 - elm0 + 1; i++) {
         let str = String(currentCheck);
-
         if (str.substring(0, 1) == "0" || /^(.+)\1{1,}$/.test(str)) {
             console.log(currentCheck);
             invalidSum += currentCheck;
